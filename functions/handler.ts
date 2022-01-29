3f2293919a1453a5b91a5eeeb61aa082203af9d6
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-01-29 20:55:51 +0530

    chore: :tada: init - serverless template
    Added offline, and bundle for typescript

---
+++ functions/handler.ts
@@ -0,0 +1,15 @@
+import express, { json, urlencoded } from "express";
+import serverless from "serverless-http";
+
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get("/hello", (_, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+export const hello = serverless(app);
