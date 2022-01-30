2fb6f982df5bb62e189ea2c717480af183a8ef81
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-01-30 13:54:51 +0530

    chore: :bug: added cors config on serverless level

--- functions/handler.ts
+++ functions/handler.ts
@@ -1,11 +1,9 @@
 import express, { json, urlencoded } from 'express';
-import cors from 'cors';
 import helmet from 'helmet';
 import serverless from 'serverless-http';
 
 const app = express();
 
-app.use(cors());
 app.use(helmet());
 
 app.use(json());
